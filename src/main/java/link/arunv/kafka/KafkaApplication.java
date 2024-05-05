89c34e68e1a799a9c8066fa19cae71bb0e99ea24
arunvemireddy <vemireddyarun0701@gmail.com>
2024-05-04 21:23:05 -0400

    reformatted code

--- src/main/java/link/arunv/kafka/KafkaApplication.java
+++ src/main/java/link/arunv/kafka/KafkaApplication.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"link.arunv.kafka"})
